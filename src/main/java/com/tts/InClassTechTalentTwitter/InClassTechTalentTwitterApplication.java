4253299b3f96b621cd8c95a2d75c7b6d3bbc4a9e
mmmobley <mobleym23@outlook.com>
2020-06-18 08:48:18 -0500

    first commit

---
+++ src/main/java/com/tts/InClassTechTalentTwitter/InClassTechTalentTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.tts.InClassTechTalentTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InClassTechTalentTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InClassTechTalentTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/InClassTechTalentTwitter/InClassTechTalentTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.InClassTechTalentTwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InClassTechTalentTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
